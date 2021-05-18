0f7562c110da4b28c74a241d21041da2576bf1ac
Ravi <ravinder9.reddy@gmail.com>
2021-05-18 22:47:53 +0530

    practiced shortcut arrow func

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -15,6 +15,18 @@ const printNameAge = (name, age) => {
     console.log(name, age)
 }
 
+const multiply = (number) => {
+    return number * 2
+}
+
+// Above function can be written as below short way
+
+const multi = number =>  number * 2
+
+console.log(multiply(5))
+
+console.log(multi(4))
+
 printHello()
 
 let name = "Ravi"
