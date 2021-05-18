731bc314413ccbdaf9e7973f434e63782e6e9e6b
Ravi <ravinder9.reddy@gmail.com>
2021-05-18 22:24:15 +0530

    Learnt JS variables

---
+++ variables.js
@@ -0,0 +1,22 @@
+// We can declare variables in 3 types:
+// 1. var - let & const both comes under var. 
+// 2. let - create variable by assigning values and then change the values if req
+// 3. const - constant variables, can't change them later, same as FINAL in java.
+
+var myName = "Ravi"
+console.log(myName)
+
+myName = "Reddy"
+console.log(myName)
+
+
+let city = "Hyd"
+console.log(city)
+
+city = "Banglore"
+console.log(city)
+
+const country = "India"
+console.log(country)
+
+// country = "Bharat"  we can't re-assign constant value. 
