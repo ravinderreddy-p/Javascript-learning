5196d93f05ac6f68d6705ffeb55d298acb1c0707
Ravi <ravinder9.reddy@gmail.com>
2021-05-18 22:41:56 +0530

    practices few more arrow func

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -6,7 +6,22 @@ const printMyName = (name) => {
     console.log(name)
 }
 
+// If passing argument is only one, then you can also write function as below.
+const printName = name => {
+    console.log(name)
+}
+
+const printNameAge = (name, age) => {
+    console.log(name, age)
+}
+
 printHello()
 
 let name = "Ravi"
+let age = "28"
+
 printMyName(name)
+
+printName(name)
+
+printNameAge(name, age)
