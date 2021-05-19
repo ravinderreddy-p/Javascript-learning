ed1dc0b47fa75e1ea7c96d94e5d0444e6e27db88
Ravi <ravinder9.reddy@gmail.com>
2021-05-19 22:16:28 +0530

    learnt if-else in js

---
+++ if-else.js
@@ -0,0 +1,12 @@
+const age = 19
+const isOldEnough = age >= 18
+
+console.log(isOldEnough)
+
+if (isOldEnough) {  //if isOldEnough == true) then it prints.
+    console.log('Vikas can start driving license 🚘') //CMD+CTRL+SPACE to get Sticker like smiley, car...etc
+}
+
+else {
+    console.log('Not permittable age')
+}
---
+++ operators.js
@@ -0,0 +1,46 @@
+
+const now = 2021 //assignment operator "="
+const future = now + 5 // addition
+
+console.log(future)
+
+const ageRavi = now - 1988 //subtraction
+const ageCharvi = now - 2018
+
+console.log(ageRavi, ageCharvi)
+
+console.log(ageRavi * 2, ageCharvi * 2) //muliplication
+
+console.log(2 ** 3) //2 to the power of 3 = 2*2*2
+
+console.log (10/3) //division
+
+const firstName = 'Ravi'
+const lastName = 'Pullagurla'
+
+console.log(firstName + lastName) //Strings concatenation
+console.log(firstName + " "+ lastName)
+
+const str = `my first name's ${firstName}, and last name's ${lastName}`
+
+console.log(str)
+
+//print outout in new lines
+console.log('hi \n\
+welcome to \n\
+Javascript')
+
+console.log(`hell,
+welcome to 
+javascript`)
+
+
+let x = 10
+x += 5 // x = x + 5
+console.log(x)
+console.log(x++) //x = x + 1
+console.log(x--) // x = x - 1
+
+//comparason operators
+console.log(ageRavi > ageCharvi)  //true
+console.log(ageCharvi > ageRavi) //false
