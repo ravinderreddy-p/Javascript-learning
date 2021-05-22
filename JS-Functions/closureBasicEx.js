efbfafa2eb25b119fa8758d885b7d64caefd3bd5
Ravi <ravinder9.reddy@gmail.com>
2021-05-23 00:44:35 +0530

    Added arrow closure function

--- JS-Functions/closureBasicEx.js
+++ JS-Functions/closureBasicEx.js
@@ -8,3 +8,9 @@ greet('Hello')('Ravi!'); //Hello Ravi!
 
 const greetHey = greet('Hey');
 greetHey('Charvi!') //Hey Charvi
+
+// I can write above greet function in arrow function
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Ravi'); //Hi Ravi
